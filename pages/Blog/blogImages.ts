64f24b59302366b14e130b4ae69402cbe196dd8d
Kinezium <hamza.chnider@gmail.com>
2026-03-17 16:05:40 +0000

    01

--- pages/Blog/blogImages.ts
+++ pages/Blog/blogImages.ts
@@ -1,12 +1,12 @@
 export const BLOG_TOPIC_IMAGES: Record<string, string> = {
-  'sciatica-herniated-disc': 'https://picsum.photos/seed/sciatica-chnider/1600/900',
-  'back-pain': 'https://picsum.photos/seed/backpain-chnider/1600/900',
+  'sciatica-herniated-disc': 'https://blogger.googleusercontent.com/img/a/AVvXsEhlOqYl31fzCBXyaeWnhyE60DnzUQCp65ljnuVB3a8BNXjFhn1KChhZSxp0rx4e0LPvqCzTgziB_CE3yQLnKKf7aQBH796_zW1Od-VL7su2znRUHwmwMQ2dtrQxNE1uFCezbdnOl4yJPx-puVyRHE9j5pOiqwfx6B32pdpqzXn13738q7xrcLSXkW02',
+  'back-pain': 'https://blogger.googleusercontent.com/img/a/AVvXsEjlQ0wlNwhkL--AQHYHr6Ev3LoMGfFRxfzPI31iT_J8DD2RhsqF4KNvVShg3ITN7l5ED0n1uAnOWp-6-IAZGhoE1866triyPkTgUJhQXYcYu54AofuHp0e2A5cL7YzVJTZDQNCGAIKTRf5lzGZT0G3wF-LLJXxMa7pKLRTqytQimKu1L6jWmWmFmK8D',
   'sports-injuries': 'https://picsum.photos/seed/sportsinjury-chnider/1600/900',
-  'knee-osteoarthritis': 'https://picsum.photos/seed/knee-chnider/1600/900',
-  'shoulder-pain': 'https://picsum.photos/seed/shoulder-chnider/1600/900',
+  'knee-osteoarthritis': 'https://blogger.googleusercontent.com/img/a/AVvXsEhLmAU4ra4i3qhedIAxc1IbMaLRyN5UkvchXfXA7lkOfra-ZLLiSCjUhnI464I_1vO829doDHkshkNteTlmsQ6eOWmvg50pMueKNZguIRXhZNvOh8jrOUTojk93DbvSl_I0i4LU1aGE5Su-pU5IwOHihTepiHhLYUQLljHaM36GxT5d3osnC6aavKAi',
+  'shoulder-pain': 'https://blogger.googleusercontent.com/img/a/AVvXsEhaDnti-UmuiTnaay3wBvFdrH5UBzFg4p6HQEuFbSGWPHHSfnWQqBGeChzK-YNnFYH_LX1iuYHUPHDOOyTZms7IsFXvFSDqbnNuj30nv4TWXnQ5WzvVKR2oK6xCe7se4x7GWSA4K-PboqVO12JHR2Jh4M4-s-hBpPETwtQQikFMuO_JzK2EPstaXof-',
   'migraines-headaches': 'https://picsum.photos/seed/migraine-chnider/1600/900',
   'pediatric-physiotherapy': 'https://picsum.photos/seed/pediatric-chnider/1600/900',
-  'post-stroke': 'https://picsum.photos/seed/poststroke-chnider/1600/900',
-  'posture-ergonomics': 'https://picsum.photos/seed/posture-chnider/1600/900',
-  'sports-recovery': 'https://picsum.photos/seed/recovery-chnider/1600/900'
+  'post-stroke': 'https://blogger.googleusercontent.com/img/a/AVvXsEjgbD0gLWSDaXNFsN4jGAxfo57PmWI3zcdN5ATvsujcg2OTEN2KrePuU5ELFzI-XdssSSkmodmPZ_F80DUsvq1d3VPiAxkoqo_GvFoD_1PTMSIxXknfMcRiyonBSMPjnTOoqUfRAls6lPVBPX6f8m6Ps8mPQTsJgseCw90r5R37qAVlTftfcIrGGgy8',
+  'posture-ergonomics': 'https://blogger.googleusercontent.com/img/a/AVvXsEjmjnzipPijYMh10L_4mFgkPe4oFb4Pr1RFw4KBgpoygO1KYKLiok1FdcIQAfNG76R_x3FjbWDesFFRQMOa630GA0d-HBb9DCSH8mmMLUxWcZ8mkH_HKx4ky--qkYIbk0O3DO5BVwmO1kZxKx1aijAA8SV81RoGv82G_6fn2ivPFnmoEOeewE_F-qZd',
+  'sports-recovery': 'https://blogger.googleusercontent.com/img/a/AVvXsEjVOBZMCgvh6xjfI8Iv6ZOvAQPLWnOPpU9arqpTUBOsocR6QzAtbtqG64Ha4q1G_2KVDvMYd27vy6l8gexMLz105SsBluWpcz1aqj-qTbVcsfTsxKVUp_KRJwpHgXzKnI3lGqdebH8iKwKvs4LEKAiSKf3mqXPHOgSiJOLkuz3vzz50y_KV-JZtFWgf'
 };
